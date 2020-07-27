781ca33da268947004538a22d7fd356ac41003b9
JeniferMo24 <jmora50299@ufide.ac.cr>
2020-07-26 22:12:04 -0600

    Avance Jenifer Inicie con la programaión de registro, aun no esta funcionando

--- Proyecto/src/main/java/controller/UsuarioController.java
+++ Proyecto/src/main/java/controller/UsuarioController.java
@@ -4,6 +4,8 @@ import gestion.UsuarioGestion;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import model.Usuario;
 
 @Named(value = "usuarioController")
@@ -31,5 +33,17 @@ public class UsuarioController extends Usuario implements Serializable {
             return "index.xhtml";
         }
 
+    }
+    
+    public String inserta() {
+        if (UsuarioGestion.insertar(this)) {
+            return "index.xhtml";  
+        } else { 
+            FacesMessage mensaje = new FacesMessage(FacesMessage.SEVERITY_ERROR,
+            "Error","Es posible que el nombre de usuario esté duplicada");
+            FacesContext.getCurrentInstance().addMessage(
+                    "editaUsuarioForm:NombreUsuario", mensaje);
+            return "Registro.xhtml";
+        }
     }
 }
