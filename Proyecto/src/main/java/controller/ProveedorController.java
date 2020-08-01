1b435aced0c9c09950c24c34adfef424b65400aa
papuale <lfallas60605@ufide.ac.cr>
2020-08-01 11:09:03 -0600

    Reparación ProveedorController (FUNCIONANDO)

--- Proyecto/src/main/java/controller/ProveedorController.java
+++ Proyecto/src/main/java/controller/ProveedorController.java
@@ -6,22 +6,25 @@
 package controller;
 
 import gestion.ProveedorGestion;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
 import java.util.List;
-import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
-import javax.inject.Named;
 import model.Proveedor;
 
 /**
  *
  * @author AleGamer
  */
-@Named(value = "proveedorController")
+@Named(value = "proveedorControllers")
 @SessionScoped
-public class ProveedorController extends Proveedor implements Serializable {
+public class ProveedorController extends Proveedor implements Serializable{
 
+    /**
+     * Creates a new instance of proveedorControllers
+     */
     public ProveedorController() {
         
     }
@@ -83,5 +86,4 @@ public class ProveedorController extends Proveedor implements Serializable {
             return "proveedor.xhtml";
         }
     }
-    
 }
