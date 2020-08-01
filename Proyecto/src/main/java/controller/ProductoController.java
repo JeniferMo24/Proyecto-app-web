afa441823c5f296a42bfa39afeacb05eb3732589
Andres-07 <aarguedas700111@ufide.ac.cr>
2020-08-01 12:01:39 -0600

    Comentario prueba

--- Proyecto/src/main/java/controller/ProductoController.java
+++ Proyecto/src/main/java/controller/ProductoController.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package controller;
-
+//Producto Controller
 import gestion.ProductoGestion;
 import gestion.ProveedorGestion;
 import javax.inject.Named;
